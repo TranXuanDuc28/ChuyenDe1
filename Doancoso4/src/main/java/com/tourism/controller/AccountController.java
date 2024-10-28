bed1c8badf52d252db7e84203a3d261c846bf1f7
LAPTOPMSI <ductx.22it@vku.udn.vn>
2024-10-28 10:47:22 +0700

    Tran Duc commit

--- Doancoso4/src/main/java/com/tourism/controller/AccountController.java
+++ Doancoso4/src/main/java/com/tourism/controller/AccountController.java
@@ -1,7 +1,6 @@
 
 package com.tourism.controller;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.tourism.dao.CustomerDAO;
 import com.tourism.entity.Customer;
-import com.tourism.service.CookieService;
 
 @Controller
 public class AccountController {
@@ -22,8 +19,6 @@ public class AccountController {
 	 CustomerDAO dao;
 	 @Autowired
 	 HttpSession session;
-	 @Autowired
-	 CookieService cookie;
 	 
 	 
 	
@@ -42,21 +37,8 @@ public class AccountController {
 		Customer user = dao.findById(id);
 		if(user == null) {
 			model.addAttribute("message", "Username không tồn tại!");
-		}else if(!pw.equals(user.getCustomer_password())) {
-			model.addAttribute("message","Mật khẩu sai!");
-		}else {
-			model.addAttribute("message", "Đăng nhập thành công!");
-			session.setAttribute("user", user);
+		}else if(!pw.equals(user.)) {
 			
-			//Ghi nho tai khoan bang cookie
-			if(rm == true) {
-				cookie.create("userId", user.getCustomer_id().toString(), 30);
-				cookie.create("pass", user.getCustomer_password(), 30);
-				
-			}else {
-				cookie.delete("userId");
-				cookie.delete("pass");
-			}
 		}
 		return "account/login";
 	}
---
+++ Doancoso4/src/main/java/com/tourism/dao/CustomerDao.java
@@ -0,0 +1,5 @@
+package com.tourism.dao;
+
+public class CustomerDao {
+
+}
